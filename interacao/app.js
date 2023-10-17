1b9b9fe8022cbaf0b611a4121aa87dc3660e54cf
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2023-10-17 12:16:26 -0300

    Initial commit

---
+++ interacao/app.js
@@ -0,0 +1,27 @@
+function carregar(){
+var msg = window.document.getElementById('msg');
+var img = window.document.getElementById('imagem');
+
+    var data = new Date()
+    var hora = data.getHours()
+
+    if(hora >= 0 && hora < 12)
+    {
+        //Bom dia
+        img.src = 'icones/dia.png'
+        msg.innerHTML = `Bom dia, prezado cliente`
+    } 
+    else if(hora >= 12 && hora < 18)
+    {
+        //Boa tarde
+        img.src = 'icones/tarde.png'
+        msg.innerHTML = `Boa tarde, prezado cliente`
+    }
+    else
+    {
+        //Boa noite
+        img.src = 'icones/noite.png'
+        msg.innerHTML = `Boa noite, prezado cliente`
+        //document.body.style.background = '#444444'
+    }
+}
